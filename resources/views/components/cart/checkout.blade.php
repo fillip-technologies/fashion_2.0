624a4c9428f27b407081caee9bc27a5837dff0e1
Mohit <mohitbite@gmail.com>
2026-01-12 13:47:41 +0530

    complete the chechout page

--- resources/views/components/cart/checkout.blade.php
+++ resources/views/components/cart/checkout.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-background py-5">
-    <div class="mx-auto px-4 sm:px-14 h-24 flex items-center text-black">
+    <div class="mx-auto px-4 sm:px-14 h-24 flex justify-between items-center text-black">
 
         <a href="/" class="flex items-center space-x-2 text-[1.25rem] cursor-pointer">
             <i class="fa-solid fa-chevron-left fa-sm text-[0.625rem]"></i>
@@ -10,13 +10,69 @@
                 <span class="text-white text-xs sm:text-sm font-semibold">LOGO 1</span>
             </div>
         </div>
+        <div>
+
+        </div>
 
     </div>
 </header>
-<div class="border-y-8 border-secondary bg-background">
-    <div class="px-4 md:px-8 py-6 md:py-8 grid grid-cols-1 md:grid-cols-12 bg-background">
+<div class="h-px w-full bg-dash-dot"></div>
 
-        <div class="md:col-span-4 bg-white relative">
+
+<div class="w-full bg-background">
+    <div class="w-10/12 mx-auto py-6 bg-background">
+        <div class="flex items-center md:grid md:grid-cols-12 md:items-center">
+            <div class="flex items-center gap-3 md:col-span-3 md:flex-none">
+                <span
+                    class="w-6 h-6 flex items-center justify-center
+                           border border-secondary rounded-full
+                           text-[0.625rem]">
+                    1
+                </span>
+                <p class="hidden md:block text-[0.875rem]">
+                    Account &amp; shipping information
+                </p>
+                <i class="block md:hidden fa-solid fa-user text-[0.875rem]"></i>
+            </div>
+            <div class="flex-1 md:flex-none md:col-span-3">
+                <div class="h-px bg-secondary w-full"></div>
+            </div>
+            <div class="flex items-center gap-3 md:col-span-2 md:flex-none justify-center">
+                <span
+                    class="w-6 h-6 flex items-center justify-center
+                           border border-secondary rounded-full
+                           text-[0.625rem] text-black/50">
+                    2
+                </span>
+                <p class="hidden md:block text-[0.875rem] text-black/50">
+                    Secure payment
+                </p>
+                <i class="block md:hidden fa-solid fa-lock text-[0.875rem] text-black/50"></i>
+            </div>
+            <div class="flex-1 md:flex-none md:col-span-2">
+                <div class="h-px bg-secondary w-full"></div>
+            </div>
+            <div class="flex items-center gap-3 md:col-span-2 md:flex-none justify-end">
+                <span
+                    class="w-6 h-6 flex items-center justify-center
+                           border border-secondary rounded-full
+                           text-[0.625rem] text-black/50">
+                    3
+                </span>
+                <p class="hidden md:block text-[0.875rem] text-black/50">
+                    Order confirmation
+                </p>
+                <i class="block md:hidden fa-solid fa-check text-[0.875rem] text-black/50"></i>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="border-y-8 border-secondary bg-background">
+    <div class="px-4 md:px-8 py-6 md:py-8 grid grid-cols-1 md:grid-cols-12 bg-background gap-4 md:gap-0">
+
+        <div class="md:col-span-4 bg-white relative self-start">
 
             <div class="absolute top-0 left-0 h-full w-px bg-dash-dot-v"></div>
 
@@ -188,7 +244,7 @@
                     <p class="text-[0.875rem] text-green-600" style="font-weight: 600">Authenticated</p>
                 </div>
 
-                <div class="border border-gray-300 rounded-md px-14 py-5 mt-5">
+                <div class="border border-gray-300 rounded-md px-6 md:px-14 py-5 mt-5">
                     <p class="text-[0.875rem] text-black py-7" style="font-weight: 400">
                         Your email -
                         <span class="ml-2" style="font-weight: 300">Yourname@domain</span>
@@ -196,7 +252,7 @@
                 </div>
             </div>
 
-            <div class="bg-background h-7"></div>
+            <div class="bg-background h-10"></div>
 
             <div class="w-10/12 mx-auto pt-12 pb-14">
 
@@ -211,7 +267,8 @@
                 </div>
 
                 <!-- Address Box -->
-                <div class="border border-secondary grid grid-cols-2 mt-5 text-[0.875rem]" style="font-weight: 300">
+                <div class="border border-secondary grid grid-cols-1 md:grid-cols-2 mt-5 text-[0.875rem]"
+                    style="font-weight: 300">
 
                     <!-- Shipping -->
                     <div class=" pt-8 border-r border-secondary">
@@ -243,7 +300,7 @@
                             </div>
                         </div>
 
-                        <div class="flex justify-between py-7 text-[0.75rem] underline border-t border-secondary px-10"
+                        <div class="flex justify-between py-7 text-[0.75rem] underline border-b md:border-b-0 md:border-t border-secondary px-10"
                             style="font-weight: 400">
                             <button>Change address</button>
                             <button>Edit address</button>
@@ -280,7 +337,7 @@
                             </div>
                         </div>
 
-                        <div class="flex justify-between py-7 text-[0.75rem] underline border-t border-secondary px-10"
+                        <div class="flex justify-between py-7 text-[0.75rem] underline md:border-t border-secondary px-10"
                             style="font-weight: 400">
                             <button>Change address</button>
                             <button>Edit address</button>
@@ -291,7 +348,7 @@
 
             <div class="border-t border-secondary"></div>
 
-            <div class="w-10/12 mx-auto pt-12 pb-20">
+            <div class="w-10/12 mx-auto pt-12 pb-24">
 
                 <p class="text-[0.75rem]" style="font-weight: 500">
                     Please enter you Postal code so we can give you the estimated delivery dates -
@@ -301,29 +358,216 @@
                     (Make sure this postal code matches the one in your shipping address of choice.)
                 </p>
 
-                <div class="flex items-end gap-10 mt-10">
 
-                    <div class="flex flex-col gap-2 w-[380px]">
+                <div class="flex flex-col md:flex-row md:items-end md:justify-between gap-4 mt-10">
+                    <div class="flex flex-col gap-2 w-full md:w-96">
                         <label class="text-[0.875rem]" style="font-weight: 400">
                             * Postal code
                         </label>
-
                         <input type="text"
                             class="border border-secondary h-11 px-4 text-[0.75rem] focus:outline-none" />
                     </div>
-
-                    <button class="h-11 px-14 bg-secondary hover:bg-primary rounded text-white text-[0.875rem] flex items-center justify-center"
+                    <button id="checkoutBtn"
+                        class="h-11 w-full md:w-auto px-14
+                        bg-secondary hover:bg-primary rounded text-white
+                        text-[0.875rem] flex items-center justify-center"
                         style="font-weight: 500">
                         Check
                     </button>
-
                 </div>
+
+
+                <div id="deliverySection" class="mx-auto pt-12 hidden">
+                    <div class="flex flex-col md:flex-row items-start md:items-center gap-3 mb-4">
+                        <p class="text-[0.875rem]">
+                            *DELIVERY OPTIONS
+                        </p>
+                        <p class="text-[0.75rem] text-secondary">
+                            (Please select you choice of dates for delivery)
+                        </p>
+                    </div>
+
+                    <div class="border border-secondary rounded-md grid grid-cols-1 md:grid-cols-2 text-[0.875rem] relative"
+                        style="font-weight: 300">
+
+                        <div class="px-10 py-8">
+
+                            <div class="flex items-start gap-4">
+                                <input type="checkbox" class="mt-1 accent-black" />
+                                <div>
+                                    <p class="text-[0.875rem]" style="font-weight: 500">Express Delivery</p>
+                                    <p class="text-[0.6875rem] mt-1">
+                                        ( Additional Charges - <span style="font-weight: 400">₹200.00</span> )
+                                    </p>
+
+                                    <p class="mt-6 text-[0.75rem]">Between</p>
+
+                                    <p class="mt-3 text-[0.75rem]" style="font-weight: 500">
+                                        5/07/2026 - 23/07/2026
+                                    </p>
+
+                                    <p class="mt-4 text-[0.75rem] text-secondary">
+                                        (Some locations might take an extra 5 - 10 days.)
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="h-px w-full bg-dash-dot md:hidden"></div>
+
+                        <div class="absolute inset-y-0 left-1/2 -translate-x-1/2 w-px bg-dash-dot-v hidden md:block">
+                        </div>
+
+                        <div class="px-10 py-8">
+
+                            <div class="flex items-start gap-4">
+                                <input type="checkbox" class="mt-1 accent-black" />
+                                <div>
+                                    <p class="text-[0.875rem]" style="font-weight: 500">Standard Delivery</p>
+                                    <p class="text-[0.6875rem] mt-1">
+                                        ( Free )
+                                    </p>
+
+                                    <p class="mt-6 text-[0.75rem]">Between</p>
+
+                                    <p class="mt-3 text-[0.75rem]" style="font-weight: 500">
+                                        17/07/2026 - 23/08/2026
+                                    </p>
+
+                                    <p class="mt-4 text-[0.75rem] text-secondary">
+                                        (Some locations might take an extra 5 - 10 days.)
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
 
-            
-            <div class="bg-background h-7"></div>
+            <div class="bg-background h-10"></div>
+
+            <div class="w-10/12 mx-auto pt-16 pb-16">
+
+                <div class="grid grid-cols-12 items-center mb-8">
+                    <div class="col-span-8 flex items-center gap-4">
+                        <span class="w-5 h-5 border border-secondary rounded-full"></span>
+                        <p class="text-[1rem] text-black/50" style="font-weight: 600">
+                            Payment details
+                        </p>
+                    </div>
+
+                    <div class="col-span-4 text-right">
+                        <p class="text-[0.875rem] text-secondary" style="font-weight: 500">
+                            Confirmed
+                        </p>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-12 gap-4">
+                    <div class="col-span-12 flex justify-end pt-1 space-x-5">
+                        <div
+                            class="w-10 h-4 flex items-center justify-center
+                            border border-secondary rounded-full
+                            text-[0.625rem] leading-none font-medium
+                            px-1">
+                            i
+                        </div>
+                        <p class="text-[0.75rem] text-secondary leading-relaxed" style="font-weight: 300">
+                            You will receive an activation code by email to confirm the creation of your account.
+                            Your personal data is processed by Louis Vuitton Malletier in order to manage and
+                            optimize the customer experience, provide information about Louis Vuitton products,
+                            services and events, as well as for surveys and statistical purposes. Our communications
+                            can be customized based on the customer’s profile and areas of interest, unless you
+                            object to such personalization. You also have the right to access, rectify, delete,
+                            limit or oppose the processing of data. You can request a copy of your data to exercise
+                            the right to portability. All these rights can be exercised at any time by sending the
+                            request to our data protection officer at: personaldata@louisvuitton.com.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="bg-background h-10"></div>
+
+            <div class="w-10/12 mx-auto pt-16 pb-16">
+
+
+                <div class="grid grid-cols-12 gap-4">
+                    <div class="col-span-12 flex justify-end pt-1 space-x-5">
+                        <div
+                            class="w-10 h-4 flex items-center justify-center
+                            border border-secondary rounded-full
+                            text-[0.625rem] leading-none font-medium
+                            px-1">
+                            i
+                        </div>
+                        <div class="space-y-1">
+                            <p class="text-[0.75rem] text-secondary leading-relaxed" style="font-weight: 300">
+                                You will receive an activation code by email to confirm the creation of your account
+                                Your
+                                personal data is processed by Louis Vuitton Malletier in order to manage and optimize
+                                the
+                                customer experience, provide information about Louis Vuitton products, services and
+                                events,
+                                as well as for surveys and statistical purposes. Our communications can be customized
+                                based
+                                on the customer's profile and areas of interest, unless you object to such
+                                personalization.
+                                You also have the right to access, rectify, delete, limit or oppose the processing of
+                                data.
+                                You can request a copy of your data to exercise the right to portability. All these
+                                rights
+                                can be exercised at any time by sending the request to our data protection officer at:
+                                personaldata@louisvuitton.com.
+                            </p>
+                            <p class="text-[0.75rem] text-secondary leading-relaxed" style="font-weight: 300">
+                                You will receive an activation code by email to confirm the creation of your account
+                                Your
+                                personal data is processed by Louis Vuitton Malletier in order to manage and optimize
+                                the
+                                customer experience, provide information about Louis Vuitton products, services and
+                                events,
+                                as well as for surveys and statistical purposes. Our communications can be customized
+                                based
+                                on the customer's profile and areas of interest, unless you object to such
+                                personalization.
+                                You also have the right to access, rectify, delete, limit or oppose the processing of
+                                data.
+                                You can request a copy of your data to exercise the right to portability. All these
+                                rights
+                                can be exercised at any time by sending the request to our data protection officer at:
+                                personaldata@louisvuitton.com.
+                            </p>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+
 
 
         </div>
     </div>
 </div>
+
+
+<script>
+    const checkoutBtn = document.getElementById("checkoutBtn");
+    const deliverySection = document.getElementById("deliverySection");
+
+    checkoutBtn.addEventListener("click", () => {
+        deliverySection.classList.remove("hidden");
+
+        deliverySection.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+</script>
