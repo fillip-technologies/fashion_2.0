7849edf2d9d49b18a2968dedfbac4daebe2bd5ac
Mohit <mohitbite@gmail.com>
2026-01-21 14:39:32 +0530

    replace the icons of the lookbook and fix the space

--- resources/views/components/lookbook.blade.php
+++ resources/views/components/lookbook.blade.php
@@ -1,11 +1,11 @@
-<section class="bg-background min-h-screen text-black relative">
-    
+<section class="bg-secondary/20 min-h-screen text-black relative">
+
     <img src="{{ asset('assets/images/lookbook/fruit.png') }}" alt="Back"
         class="w-[120px] h-[120px] md:h-[140px] absolute -top-5 left-10 md:left-56" />
 
     <div class="px-6 md:px-14 pt-14">
-        <div class="mb-10">
-            <img src="{{ asset('assets/icons/open-angular.png') }}" alt="Back" class="w-[7px] h-[12px]" />
+        <div class="mb-10 cursor-pointer" onclick="window.history.back()">
+            <img src="{{ asset('assets/SVG/Back black icon.svg') }}" alt="Back" class="w-[0.4375rem] h-[0.75rem]" />
         </div>
         <div class="max-w-2xl mx-auto flex flex-col items-center justify-center text-center mb-10 md:mb-16">
             <h2 class="text-[20px] tracking-[0.15em] font-medium mb-2 uppercase">
@@ -16,7 +16,7 @@
         </div>
 
 
-        <div class="space-y-10 max-w-3xl mx-auto text-[14px] leading-relaxed text-justify" style="font-weight: 200">
+        <div class="space-y-10 py-10 max-w-3xl mx-auto text-[0.875rem] leading-relaxed text-justify" style="font-weight: 200">
             <div>
                 A fashion collection is a curated group of clothing, footwear, and accessories designed around a central
                 theme, concept, or inspiration, unified by shared colors, fabrics, silhouettes, and styles for a
@@ -69,7 +69,7 @@
         </div>
     </div>
 
-    
+
     <div class="mt-12 grid grid-cols-1 md:grid-cols-2
           mx-auto min-h-[520px] relative">
 
