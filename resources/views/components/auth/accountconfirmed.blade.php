1ebbd0306d30896b87a1db21a8d74f7f7097d99c
Mohit <mohitbite@gmail.com>
2026-01-23 14:26:51 +0530

    refactor navigation icons and improve layout in account confirmed and confirm order components

--- resources/views/components/auth/accountconfirmed.blade.php
+++ resources/views/components/auth/accountconfirmed.blade.php
@@ -3,9 +3,10 @@
     <header class="relative border-b bg-[rgba(151,160,161,0.5)] backdrop-blur-[44px] z-20">
         <div class="mx-auto px-4 sm:px-14 h-24 flex items-center text-white">
 
-            <a href="/" class="flex items-center space-x-2 text-[0.7rem] sm:text-[0.8125rem] cursor-pointer">
-                <i class="fa-solid fa-arrow-left fa-sm"></i>
-                <span>Back to browsing!</span>
+            <a href="/" class="flex items-center space-x-2 text-[0.8125rem] cursor-pointer">
+                {{-- <i class="fa-solid fa-arrow-left fa-sm"></i> --}}
+                <img src="{{ asset('assets/SVG/Return white icon.svg') }}" alt="Arrow Left" class="w-[7px] h-[6px]" />
+                <span class="hidden md:block" style="font-weight: 100">Back to browsing!</span>
             </a>
             <div class="mx-auto">
                 <div class="w-16 h-16 sm:w-24 sm:h-24 bg-purple-600 flex items-center justify-center">
@@ -13,10 +14,14 @@
                 </div>
             </div>
 
-            <div class="flex items-center space-x-1 sm:space-x-0 text-[0.7rem] sm:text-[0.8125rem] cursor-pointer">
-                <span class="mr-1 sm:mr-2">Go to your profile</span>
-                <i class="fa-solid fa-chevron-right fa-sm"></i>
-                <i class="fa-solid fa-chevron-right fa-sm"></i>
+            <div class="flex items-center text-[0.8125rem] cursor-pointer">
+                <span class="mr-1 sm:mr-2 hidden md:block" style="font-weight: 300">Go to your profile</span>
+                {{-- <i class="fa-solid fa-chevron-right fa-sm"></i>
+                <i class="fa-solid fa-chevron-right fa-sm"></i> --}}
+                <img src="{{ asset('assets/SVG/Back white icon.svg') }}" alt="Arrow Left"
+                    class="w-[7px] h-[6px] rotate-180" />
+                <img src="{{ asset('assets/SVG/Back white icon.svg') }}" alt="Arrow Left"
+                    class="w-[7px] h-[6px] rotate-180 -ml-0.5" />
             </div>
 
         </div>
@@ -31,8 +36,8 @@
     <div class="absolute top-0 left-0 w-full h-full bg-black/30 z-5"></div>
 
     <div class="absolute inset-0 flex flex-col items-center justify-center text-center z-10 text-white px-4">
-        <h1 class="text-[1.5625rem] mb-2" style="font-weight: 300">Welcome to Brand name </h1>
-        <p class="text-[1.5625rem]" style="font-weight: 300">Ms. Name Surname</p>
+        <h1 class="text-[1.5625rem] mb-2" style="font-weight: 200">Welcome to Brand name </h1>
+        <p class="text-[1.5625rem]" style="font-weight: 200">Ms. Name Surname</p>
     </div>
 
 </div>
